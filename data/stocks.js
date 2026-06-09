30a639a482f2ac5d63c2fa7245b151a83742964b
无名 <wuming@hermes.local>
2026-06-09 13:41:34 +0800

    Auto update: 2026-06-09

--- data/stocks.js
+++ data/stocks.js
@@ -1,15 +1,15 @@
 const STOCKS = [
-  { "code": "688981", "name": "中芯国际", "base_price": 126.08 },
-  { "code": "688256", "name": "寒武纪", "base_price": 1274.3 },
-  { "code": "002371", "name": "北方华创", "base_price": 612.71 },
-  { "code": "688012", "name": "中微公司", "base_price": 284.73 },
-  { "code": "603501", "name": "韦尔股份", "base_price": 93.31 },
-  { "code": "603986", "name": "兆易创新", "base_price": 493.99 },
-  { "code": "688041", "name": "海光信息", "base_price": 265.21 },
-  { "code": "600584", "name": "长电科技", "base_price": 75.57 },
-  { "code": "002049", "name": "紫光国微", "base_price": 73.65 },
-  { "code": "000063", "name": "中兴通讯", "base_price": 39.03 }
+  { "code": "688981", "name": "中芯国际", "base_price": 126.06 },
+  { "code": "688256", "name": "寒武纪", "base_price": 1271.1 },
+  { "code": "002371", "name": "北方华创", "base_price": 610.32 },
+  { "code": "688012", "name": "中微公司", "base_price": 282.83 },
+  { "code": "603501", "name": "韦尔股份", "base_price": 93.02 },
+  { "code": "603986", "name": "兆易创新", "base_price": 492.83 },
+  { "code": "688041", "name": "海光信息", "base_price": 264.0 },
+  { "code": "600584", "name": "长电科技", "base_price": 74.91 },
+  { "code": "002049", "name": "紫光国微", "base_price": 73.28 },
+  { "code": "000063", "name": "中兴通讯", "base_price": 38.86 }
 ];
-const LATEST_PRICES = {"688981": {"price": 126.08, "change_pct": 3.41, "volume": 50252095.0, "prev_close": 121.92}, "688256": {"price": 1274.3, "change_pct": 1.91, "volume": 6752493.0, "prev_close": 1250.36}, "002371": {"price": 612.71, "change_pct": 4.6, "volume": 6919170.0, "prev_close": 585.74}, "688012": {"price": 284.73, "change_pct": 7.26, "volume": 15049588.0, "prev_close": 265.47}, "603501": {"price": 93.31, "change_pct": 1.4, "volume": 11572146.0, "prev_close": 92.02}, "603986": {"price": 493.99, "change_pct": 4.22, "volume": 28838309.0, "prev_close": 474.01}, "688041": {"price": 265.21, "change_pct": 2.12, "volume": 20424979.0, "prev_close": 259.7}, "600584": {"price": 75.57, "change_pct": 7.5, "volume": 124887407.0, "prev_close": 70.3}, "002049": {"price": 73.65, "change_pct": 5.24, "volume": 21111193.0, "prev_close": 69.98}, "000063": {"price": 39.03, "change_pct": 6.03, "volume": 238461731.0, "prev_close": 36.81}};
+const LATEST_PRICES = {"688981": {"price": 126.06, "change_pct": 3.4, "volume": 54844240.0, "prev_close": 121.92}, "688256": {"price": 1271.1, "change_pct": 1.66, "volume": 7458650.0, "prev_close": 1250.36}, "002371": {"price": 610.32, "change_pct": 4.2, "volume": 7308740.0, "prev_close": 585.74}, "688012": {"price": 282.83, "change_pct": 6.54, "volume": 16724061.0, "prev_close": 265.47}, "603501": {"price": 93.02, "change_pct": 1.09, "volume": 12640946.0, "prev_close": 92.02}, "603986": {"price": 492.83, "change_pct": 3.97, "volume": 30421754.0, "prev_close": 474.01}, "688041": {"price": 264.0, "change_pct": 1.66, "volume": 22050539.0, "prev_close": 259.7}, "600584": {"price": 74.91, "change_pct": 6.56, "volume": 134544759.0, "prev_close": 70.3}, "002049": {"price": 73.28, "change_pct": 4.72, "volume": 22444803.0, "prev_close": 69.98}, "000063": {"price": 38.86, "change_pct": 5.57, "volume": 247174131.0, "prev_close": 36.81}};
 const PRED_COUNTS = {};
-const LAST_UPDATE = "2026-06-09 13:24";
+const LAST_UPDATE = "2026-06-09 13:41";
