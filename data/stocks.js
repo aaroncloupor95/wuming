d73cc2657f2c768ee9db0ef9e615f7cd758c2d2f
无名 <wuming@hermes.local>
2026-07-03 01:58:49 +0800

    Auto update: 2026-07-03

---
+++ data/stocks.js
@@ -0,0 +1,15 @@
+const STOCKS = [
+  { "code": "688981", "name": "中芯国际", "base_price": 144.1 },
+  { "code": "688256", "name": "寒武纪", "base_price": 1372.0 },
+  { "code": "002371", "name": "北方华创", "base_price": 841.82 },
+  { "code": "688012", "name": "中微公司", "base_price": 408.9 },
+  { "code": "603501", "name": "韦尔股份", "base_price": 101.27 },
+  { "code": "603986", "name": "兆易创新", "base_price": 694.81 },
+  { "code": "688041", "name": "海光信息", "base_price": 322.29 },
+  { "code": "600584", "name": "长电科技", "base_price": 95.98 },
+  { "code": "002049", "name": "紫光国微", "base_price": 83.55 },
+  { "code": "000063", "name": "中兴通讯", "base_price": 35.32 }
+];
+const LATEST_PRICES = {"688981": {"price": 144.1, "change_pct": -6.72, "volume": 94902364.0, "prev_close": 154.48}, "688256": {"price": 1372.0, "change_pct": -7.63, "volume": 14056428.0, "prev_close": 1485.33}, "002371": {"price": 841.82, "change_pct": -10.0, "volume": 12121011.0, "prev_close": 935.36}, "688012": {"price": 408.9, "change_pct": -11.49, "volume": 43639954.0, "prev_close": 462.0}, "603501": {"price": 101.27, "change_pct": 4.28, "volume": 73251923.0, "prev_close": 97.11}, "603986": {"price": 694.81, "change_pct": -10.0, "volume": 45243641.0, "prev_close": 772.01}, "688041": {"price": 322.29, "change_pct": -7.89, "volume": 39804681.0, "prev_close": 349.9}, "600584": {"price": 95.98, "change_pct": -10.0, "volume": 190804372.0, "prev_close": 106.64}, "002049": {"price": 83.55, "change_pct": -3.31, "volume": 51702449.0, "prev_close": 86.41}, "000063": {"price": 35.32, "change_pct": -5.0, "volume": 182467540.0, "prev_close": 37.18}};
+const PRED_COUNTS = {};
+const LAST_UPDATE = "2026-07-03 01:58";
