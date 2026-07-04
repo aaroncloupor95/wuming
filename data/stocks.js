f66186f3398fe8fdcd370ff3563e9abab3874a03
无名 <wuming@hermes.local>
2026-07-04 16:00:36 +0800

    Auto update: 2026-07-04

--- data/stocks.js
+++ data/stocks.js
@@ -12,4 +12,4 @@ const STOCKS = [
 ];
 const LATEST_PRICES = {"688981": {"price": 140.31, "change_pct": -2.63, "volume": 77041178.0, "prev_close": 144.1}, "688256": {"price": 1353.0, "change_pct": -1.38, "volume": 10192184.0, "prev_close": 1372.0}, "002371": {"price": 816.0, "change_pct": -2.98, "volume": 16585395.0, "prev_close": 841.06}, "688012": {"price": 413.69, "change_pct": 1.17, "volume": 33812280.0, "prev_close": 408.9}, "603501": {"price": 102.3, "change_pct": 1.02, "volume": 44021645.0, "prev_close": 101.27}, "603986": {"price": 677.77, "change_pct": -2.45, "volume": 62352215.0, "prev_close": 694.81}, "688041": {"price": 325.4, "change_pct": 0.96, "volume": 29085423.0, "prev_close": 322.29}, "600584": {"price": 90.88, "change_pct": -5.31, "volume": 178226527.0, "prev_close": 95.98}, "002049": {"price": 81.76, "change_pct": -2.14, "volume": 38145194.0, "prev_close": 83.55}, "000063": {"price": 36.38, "change_pct": 3.0, "volume": 153866953.0, "prev_close": 35.32}};
 const PRED_COUNTS = {};
-const LAST_UPDATE = "2026-07-03 16:00";
+const LAST_UPDATE = "2026-07-04 16:00";
